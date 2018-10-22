688cacfc425e47a436023b96454e21d9485e3aa1
Rodolphe Pineau <pineau@rti-zone.org>
2018-10-21 20:16:01 -0700

    Zap gremlins (bad characters).

--- PDMRotator/RemoteShutterClass.h
+++ PDMRotator/RemoteShutterClass.h
@@ -1,7 +1,7 @@
 /*
 * PDM NexDome Rotation kit firmware. NOT compatible with original NexDome ASCOM driver.
 *
-* Copyright © 2018 Patrick Meloy
+* Copyright (c) 2018 Patrick Meloy
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
 *  files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy,
 *  modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
